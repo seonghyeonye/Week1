0bffd70a79f2d8f3f97c4ebaf20f344e179b08c1
서다빈 <44111674+lemonstar99@users.noreply.github.com>
2019-07-02 00:15:46 +0900

    Update FullView.java

--- app/src/main/java/com/example/newfinal/FullView.java
+++ app/src/main/java/com/example/newfinal/FullView.java
@@ -10,9 +10,9 @@ public class FullView extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.frag2_image_fullview);
+        setContentView(R.layout.activity_full_image);
 
-        ImageView imageView = findViewById(R.id.img);
+        ImageView imageView = findViewById(R.id.imageView);
         int img_id = getIntent().getExtras().getInt("img_id");
 
         imageView.setImageResource(img_id);
